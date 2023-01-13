730ff568d8cb5136b0c50811ec5220b91a652dc4
marcthiery <marcthiery@outlook.com>
2023-01-13 17:01:54 +0100

    Created user model

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * @route   GET api/users
+ * @desc    Test route
+ * @access  Public
+ */
+router.get('/', function (req, res) {
+  res.send('User route');
+});
+
+module.exports = router;
